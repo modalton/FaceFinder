a114e1fa6366b17f9b1539976bf36733befe1885
Michael Dalton <modalton@usc.edu>
2016-12-28 17:16:35 -0600

    basic face detector from sample. Need to add tracking and image output in local directory

--- OpenCVtest/Source.cpp
+++ OpenCVtest/Source.cpp
@@ -12,12 +12,16 @@ using namespace cv;
 
 String face_xml = "C:/Users/Michael Dalton/Desktop/opencv/sources/data/haarcascades/haarcascade_frontalface_alt.xml";
 CascadeClassifier face_cascade;
+void overlay(Mat frame);
 
 //Program start
 int main(void)
 {
 	VideoCapture feed(0);
 	Mat frame;
+
+	//Load our pre-made xml
+	if (!face_cascade.load(face_xml)) { std::cerr << "Error Loading face_xml\n"; return -1; };
 
 	if (!feed.isOpened()) { std::cerr << "Error opening webcam\n"; return -1; }
 
@@ -38,7 +42,7 @@ int main(void)
 			break;
 		}
 
-		imshow("Test", frame);
+		overlay(frame);
 
 
 		//Manual way to break our loop if stuck
@@ -46,4 +50,22 @@ int main(void)
 		if (c == 27) { break; } 
 	}
 	return 0;
+}
+
+void overlay(Mat frame)
+{
+	std::vector<Rect> faces;
+	Mat frame_gray;
+	cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+	equalizeHist(frame_gray, frame_gray);
+	//-- Detect faces
+	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+	for (size_t i = 0; i < faces.size(); i++)
+	{
+		Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
+		ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
+		Mat faceROI = frame_gray(faces[i]);
+	}
+	//-- Show what you got
+	imshow("Test", frame);
 }
