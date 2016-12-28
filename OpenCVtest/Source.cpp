a98bc3f8bb89af08a0b29b0336a34fc88234e8f2
Michael Dalton <modalton@usc.edu>
2016-12-28 13:16:03 -0600

    Add project files.

---
+++ OpenCVtest/Source.cpp
@@ -0,0 +1,15 @@
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace cv;
+
+
+
+int main(int, char** argv)
+{
+
+}
